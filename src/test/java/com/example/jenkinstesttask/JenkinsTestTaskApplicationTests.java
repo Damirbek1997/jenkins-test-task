43473ac4f2f90ba3838e16cb666ab255cd4ad571
dabdybapuulu@nurtelecom.kg <dabdybapuulu@nurtelecom.kg>
2023-10-16 21:43:40 +0600

    Uncommited main class test

--- src/test/java/com/example/jenkinstesttask/JenkinsTestTaskApplicationTests.java
+++ src/test/java/com/example/jenkinstesttask/JenkinsTestTaskApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.jenkinstesttask;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JenkinsTestTaskApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.jenkinstesttask;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JenkinsTestTaskApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
