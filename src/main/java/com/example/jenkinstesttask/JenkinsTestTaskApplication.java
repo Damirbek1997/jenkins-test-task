f1c80e8b0329aff6cfe14e8261d1d2ec5e89a25f
dabdybapuulu@nurtelecom.kg <dabdybapuulu@nurtelecom.kg>
2023-10-16 21:42:24 +0600

    Commit main class test

--- src/main/java/com/example/jenkinstesttask/JenkinsTestTaskApplication.java
+++ src/main/java/com/example/jenkinstesttask/JenkinsTestTaskApplication.java
@@ -1,11 +1,11 @@
-package com.example.jenkinstesttask;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JenkinsTestTaskApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(JenkinsTestTaskApplication.class, args);
-    }
-}
+//package com.example.jenkinstesttask;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class JenkinsTestTaskApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(JenkinsTestTaskApplication.class, args);
+//    }
+//}
